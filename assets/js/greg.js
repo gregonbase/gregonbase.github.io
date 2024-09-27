ac628f66b94e0615e26d955d91c0f512cccaa193
gregonbase <endispiri2@gmail.com>
2024-09-27 20:02:52 +0200

    Update greg.js
    Update CA

--- assets/js/greg.js
+++ assets/js/greg.js
@@ -1,7 +1,7 @@
 const comingSoon = document.getElementById('comingSoon');
 const caBtn = document.getElementById('ca-btn');
 const aboutGreg = document.getElementById("aboutGreg");
-const textToCopy = "0x4d5a8915EDa28a0a67cE68CF8BeAF796ee9f517e";
+const textToCopy = "";
 
 comingSoon.addEventListener("click", () => {
     alert("Coming soon, stay tuned...");
@@ -9,5 +9,5 @@ comingSoon.addEventListener("click", () => {
 
 caBtn.addEventListener("click", () => {
     navigator.clipboard.writeText(textToCopy);
-    alert("Contract address succesfully copied!");
+    alert("To be announced!");
 });
